1f2eafcd76a41406956bf104e9d29d2156863a90
Aditya K Kamath <a_kamath@hotmail.com>
2023-07-19 14:36:28 +0000

    Add missing python config for MCSquare SimObject

---
+++ src/mem/MCSquare.py
@@ -0,0 +1,10 @@
+from m5.params import *
+from m5.SimObject import SimObject
+
+class MCSquare(SimObject):
+    type = 'MCSquare'
+    cxx_header = "mem/mcsquare.h"
+    cxx_class = "gem5::memory::MCSquare"
+
+    table_size = Param.Int(2048, "Number of entries that elision table can hold")
+    table_penalty = Param.Latency("20ns", "Cycle penalty for reading a data copy that was elided")
