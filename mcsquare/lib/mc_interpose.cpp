c051812369076adff8c7e0530de3e584f3e9a840
Aditya K Kamath <a_kamath@hotmail.com>
2023-08-19 05:32:07 +0000

    Correct working of mc_interpose

--- mcsquare/lib/mc_interpose.cpp
+++ mcsquare/lib/mc_interpose.cpp
@@ -36,15 +36,14 @@
 #include <unordered_map>
 #include "mcsquare.h"
 
-#define OPT_THRESHOLD 255
-
-static inline void ensure_init(void);
+#define OPT_THRESHOLD 1023
 
 static void *(*libc_memcpy)(void *dest, const void *src, size_t n);
-static void *(*libc_malloc)(size_t size);
-static void (*libc_free)(void *ptr);
+//static void *(*libc_malloc)(size_t size) = NULL;
+//static void (*libc_free)(void *ptr) = NULL;
+//static int (*libc_munmap)(void *addr, size_t length) = NULL;
 
-std::unordered_map<void *, size_t> allocs;
+//std::unordered_map<void *, size_t> allocs;
 
 static void memcpy_elide_clwb(void* dest, const void* src, uint64_t len)
 {
@@ -66,58 +65,29 @@ static void memcpy_elide_clwb(void* dest, const void* src, uint64_t len)
         libc_memcpy(dest, src, left_fringe);
         dest = (void *)((char *)dest + left_fringe);
         temp_src = ((uint64_t)src + left_fringe);
+        len -= left_fringe;
     }
     while(len > 0) {
         // Calculate remaining size in page for src and dest
-        uint64_t src_off = PAGE_SIZE - ((uint64_t)src & (PAGE_SIZE - 1));
+        uint64_t src_off = PAGE_SIZE - ((uint64_t)temp_src & (PAGE_SIZE - 1));
         uint64_t dest_off = PAGE_SIZE - ((uint64_t)dest & (PAGE_SIZE - 1));
         // Pick minimum size left as elide_size
         uint64_t elide_size = cust_min(cust_min(src_off, dest_off), len);
-        if(elide_size < CL_SIZE)
+        if(elide_size < CL_SIZE) {
+          if(len >= CL_SIZE)
+            elide_size = CL_SIZE;
+          else
+            elide_size = len;
           libc_memcpy(dest, (void*)temp_src, elide_size);
+        }
         else {
           // Make elide size a multiple of 64
           elide_size &= (~63);
+          //fprintf(stderr, "Elide %p 0x%lx %lu\n", dest, temp_src, elide_size);
           m5_memcpy_elide(dest, (void*)temp_src, elide_size);
         }
         dest = (void *)((char *)dest + elide_size);
-        temp_src = ((uint64_t)src + elide_size);
-        len -= elide_size;
-    }
-    _mm_mfence();
-}
-
-static void memcpy_elide_clwb_huge(void* dest, const void* src, uint64_t len)
-{
-    uint64_t temp_src = ((uint64_t)src & ~((uint64_t)63));
-    while(temp_src < (uint64_t)src + len) {
-        _mm_clwb( (void*)temp_src );
-        temp_src += CL_SIZE;
-    }
-    _mm_mfence();
-    // Cacheline-align dest
-    uint64_t left_fringe = CL_SIZE - ((uint64_t)dest & (CL_SIZE - 1));
-    temp_src = (uint64_t)src;
-    if(left_fringe < CL_SIZE) {
-        libc_memcpy(dest, src, left_fringe);
-        dest = (void *)((char *)dest + left_fringe);
-        temp_src = ((uint64_t)src + left_fringe);
-    }
-    while(len > 0) {
-        // Calculate remaining size in page for src and dest
-        uint64_t src_off = HUGE_PAGE_SIZE - ((uint64_t)src & (HUGE_PAGE_SIZE - 1));
-        uint64_t dest_off = HUGE_PAGE_SIZE - ((uint64_t)dest & (HUGE_PAGE_SIZE - 1));
-        // Pick minimum size left as elide_size
-        uint64_t elide_size = cust_min(cust_min(src_off, dest_off), len);
-        if(elide_size < CL_SIZE)
-          libc_memcpy(dest, (void*)temp_src, elide_size);
-        else {
-          // Make elide size a multiple of 64
-          elide_size &= (~63);
-          m5_memcpy_elide(dest, (void*)temp_src, elide_size);
-        }
-        dest = (void *)((char *)dest + elide_size);
-        temp_src = ((uint64_t)src + elide_size);
+        temp_src = (temp_src + elide_size);
         len -= elide_size;
     }
     _mm_mfence();
@@ -128,7 +98,7 @@ static void memcpy_elide_free(void* dest, uint64_t len)
     uint64_t temp_dest = ((uint64_t)dest & ~((uint64_t)63));
     while(temp_dest < (uint64_t)dest + len) {
         _mm_clwb( (void*)temp_dest );
-        temp_dest += CL_SIZE;
+        temp_dest += PAGE_SIZE;
     }
     _mm_mfence();
     while(len > 0) {
@@ -137,6 +107,7 @@ static void memcpy_elide_free(void* dest, uint64_t len)
         // Pick minimum size left as elide_size
         uint64_t free_size = cust_min(dest_off, len);
         m5_memcpy_elide_free(dest, free_size);
+        //fprintf(stderr, "Freed %p (%lu)\n", dest, free_size);
         dest = (void *)((char *)dest + free_size);
         len -= free_size;
     }
@@ -144,8 +115,6 @@ static void memcpy_elide_free(void* dest, uint64_t len)
 }
 
 void *memcpy(void *dest, const void *src, size_t n) {
-  ensure_init();
-
   if ((n <= OPT_THRESHOLD)) {
     return libc_memcpy(dest, src, n);
   }
@@ -158,11 +127,10 @@ void *memcpy(void *dest, const void *src, size_t n) {
     }
     return libc_memcpy(dest, src, n);
   }
-
   memcpy_elide_clwb(dest, src, n);
   return dest;
 }
-
+/*
 void *malloc(size_t size) {
   ensure_init();
   // Avoid recursive calls here by setting flags
@@ -192,11 +160,12 @@ void *malloc(size_t size) {
 void free(void *ptr) {
   ensure_init();
   if(allocs.find(ptr) != allocs.end()) {
-    fprintf(stderr, "Free found alloced ptr %p, size %ld\n", ptr, allocs[ptr]);
+    //fprintf(stderr, "Free found alloced ptr %p, size %ld\n", ptr, allocs[ptr]);
+    memcpy_elide_free(ptr, allocs[ptr]);
     allocs.erase(ptr);
   }
   return libc_free(ptr);
-}
+}*/
 
 /******************************************************************************/
 /* Helper functions */
@@ -214,33 +183,18 @@ static void init(void) {
   fprintf(stderr, "MCSquare start\n");
 
   libc_memcpy = (void* (*)(void*, const void*, long unsigned int))bind_symbol("memcpy");
-  libc_malloc = (void* (*)(size_t))bind_symbol("malloc");
-  libc_free   = (void  (*)(void *))bind_symbol("free");
+  //libc_malloc = (void* (*)(size_t))bind_symbol("malloc");
+  //libc_free   = (void  (*)(void *))bind_symbol("free");
+  //libc_munmap = (int   (*)(void *addr, size_t length))bind_symbol("munmap");;
 
-  fprintf(stderr, "Memcpy %p, malloc %p\n", libc_memcpy, libc_malloc);
+  //fprintf(stderr, "Memcpy %p, malloc %p\n", libc_memcpy, libc_malloc);
 }
 
-static inline void ensure_init(void) {
-  static volatile uint32_t init_cnt = 0;
-  static volatile uint8_t init_done = 0;
-  static __thread uint8_t in_init = 0;
-
-  if (init_done == 0) {
-    /* during init the socket functions will be used to connect to the kernel
-     * on a unix socket, so make sure that runs through. */
-    if (in_init) {
-      return;
-    }
-
-    if (__sync_fetch_and_add(&init_cnt, 1) == 0) {
-      in_init = 1;
-      init();
-      in_init = 0;
-      init_done = 1;
-    } else {
-      while (init_done == 0) {
-        sched_yield();
-      }
-    }
+struct setup_handler {
+  ~setup_handler() {
+    memcpy_elide_free(this, 1);
   }
-}
+  setup_handler() {
+    init();
+  }
+} dummy;
--- mcsquare/lib/test.cpp
+++ mcsquare/lib/test.cpp
@@ -2,7 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <cstdint>
-#define MAX_SIZE (4096 * 4096)
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define MAX_SIZE (4096 * 4)
 
 int main() {
     void *dest = malloc(MAX_SIZE);
@@ -14,7 +20,12 @@ int main() {
     free(dest);
     dest = malloc(MAX_SIZE);
     src = malloc(MAX_SIZE);
-    for(int size = 32; size < MAX_SIZE; size *= 2)
+    for(int size = 4096; size <= MAX_SIZE; size *= 2) {
         memcpy(dest, src, size);
+        printf("%p %p %lu\n", dest, src, size);
+    }
+    void *addr = mmap(NULL, MAX_SIZE, PROT_READ,
+                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    munmap(addr, MAX_SIZE);
     return 0;
 }
